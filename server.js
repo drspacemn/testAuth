3e7d7cea7e2ba0a966d26789a6e3f84211d14386
Jerry Goebel <jerrygoebel@Jerrys-MacBook-Pro.local>
2016-12-05 21:42:39 -0700

    MONGO URL

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var clientController = require('./controllers/client');
 var oauth2Controller = require('./controllers/oauth2');
 var loginController = require('./controllers/login')
 
-mongoose.connect('mongodb://localhost:27017/app')
+mongoose.connect('mongodb://heroku_lsx2c8b3:ej8khtb7d47genk2sg9pksjsds@ds031681.mlab.com:31681/heroku_lsx2c8b3')
 
 // Create our Express application
 var app = express();
